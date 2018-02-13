88200b55eeebcb39fe8d916d96e96fd88432e122
Mike Morrone <Hanegwen725@gmail.com>
2018-02-13 11:05:47 -0600

    Full Main Menu Tutorial

---
+++ Assets/QuitOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour
+{
+    public void Quit()
+    {
+#if Unity_Editor
+        Unity_Editor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
